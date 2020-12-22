dcbf5a936e7f4a52a5cae8c7103ba8d77e143f8b
iafonkin <iafonkin@gmail.com>
2020-12-22 17:53:54 +0300

    Исправлены синтаксис. Убран баг с nil

--- ADX_TV.lua
+++ ADX_TV.lua
@@ -1,5 +1,5 @@
 Settings = {
-    Name = "ADX_TV", 
+    Name = "zxADX_TV", 
     Period = 14, 
     
     line = {	{
@@ -41,23 +41,31 @@ Settings = {
     end
     
     function OnCalculate(Index)
-    P = Settings.Period
+        P = Settings.Period
         if CandleExist(Index - P *2) then
-            data(Index)
+            if Index > (P * 2) then
+            
+            ADXg, DIplusg, DIminusg = data(Index)
+            end
                   
     
-        return tonumber(Settings.Horizontal_line), ADX, DIplus, DIminus
+        return tonumber(Settings.Horizontal_line), ADXg, DIplusg, DIminusg
         end
         
     end
     
     function data(Index)
         P = Settings.Period
+        TR={}
+        DMplus={}
+        DMminus={}
+        DXi={}
 
         for i = 1, P*2 do
     
             x = Index - 1 - (P * 2) + i
                 
+            if CandleExist(x) and CandleExist(x-1) then
                 TR[i] = math.max( math.max (H(x) - L(x), H(x) - L(x - 1)), math.abs(L(x)-C(x -1)) ) 
                 
                 if H(x) - H(x -1) > L(x -1) - L(x) then
@@ -72,19 +80,20 @@ Settings = {
                     DMminus[i] = 0	
                 end
                
-            if i > P then
+                if i > P then
 
-                DIplus = ssma(DMplus) / ssma(TR) * 100
-                DIminus = ssma(DMminus) / ssma(TR) * 100
-                DXi[i-P] = math.abs(DIplus - DIminus) / (DIplus + DIminus) * 100
-                
+                    DIplus = ssma(DMplus) / ssma(TR) * 100
+                    DIminus = ssma(DMminus) / ssma(TR) * 100
+                    DXi[i-P] = math.abs(DIplus - DIminus) / (DIplus + DIminus) * 100
+                    
+                end
             end
-        
         end
         
         DIplus = ssma(DMplus) / ssma(TR) * 100
         DIminus = ssma(DMminus) / ssma(TR) * 100
         ADX = sma(DXi)
+        return DIplus, DIminus, ADX
 
     end
     
@@ -92,20 +101,25 @@ Settings = {
     function ssma(tab)
         P = Settings.Period
         local sum1 = 0
+        local sum2 = 0
+        local j = 0
         
     
         for i,v in ipairs(tab) do
+
             if i <= P then
                 sum1 = sum1 + v
-                sum1 = sum1 / (P/2)
-    
-            else
-    
-                sum1 = sum1
-                sum1 = ( (sum1*(P-1) + v) / P   
+             else
+                if j==0 then
+                    sum2 = sum1 / P
+                    j=1
+                end
+
+                sum2 = (sum2*(P-1) + v) / P
+            end  
         
         end
-    return sum1
+        return sum2
     end
 
     function sma(tab)
@@ -113,7 +127,8 @@ Settings = {
         for i,v in ipairs(tab) do
             sum = sum + v
         end
-    result sum/#tab
+        result = sum/#tab
+        return result
     end
     
     
--- _z.lua
+++ _z.lua
@@ -1,156 +1,129 @@
 Settings = {
-Name = "ADX_SSMA", 
-Period = 14, 
+    Name = "zzzADX_TV1", 
+    Period = 14, 
+    
+    line = {	{
+                Name = "Horizontal line",
+                Type = TYPE_LINE, 
+                Color = RGB(0, 0, 255)
+                },
+    
+                {
+                Name = "ADX", 
+                Type = TYPE_LINE, 
+                Color = RGB(0, 162, 232)
+                },
+                
+                {
+                Name = "ADX +DI",
+                Type = TYPE_LINE, 
+                Color = RGB(0, 206, 0)
+                },
+                
+                {
+                Name = "ADX -DI",
+                Type = TYPE_LINE,
+                Color = RGB(221, 44, 44)
+                }
+            },
+    
+    Horizontal_line="35"
+    }
+    
+    TR={}
+    DMplus={}
+    DMminus={}
+    DXi={}
+    
+    function Init()
+        
+        return #Settings.line
+    end
+    
+    function OnCalculate(Index)
+        P = Settings.Period
+        if CandleExist(Index - P *2) then
+            
+          
+              
+			P = Settings.Period
+			TR={}
+			DMplus={}
+			DMminus={}
+			DXi={}
 
-line = {	{
-			Name = "Horizontal line",
-			Type = TYPE_LINE, 
-			Color = RGB(0, 0, 255)
-			},
-
-			{
-            Name = "ADX", 
-            Type = TYPE_LINE, 
-            Color = RGB(0, 162, 232)
-			},
-			
-            {
-            Name = "ADX +DI",
-            Type = TYPE_LINE, 
-            Color = RGB(0, 206, 0)
-			},
-			
-            {
-            Name = "ADX -DI",
-            Type = TYPE_LINE,
-			Color = RGB(221, 44, 44)
-			}
-		},
-
-Horizontal_line="35"
-}
-
-TR={}
-DMplus={}
-DMminus={}
-
-function Init()
-	
-	return #Settings.line
-end
-
-function OnCalculate(Index)
-P = Settings.Period
-	if CandleExist(Index - P *2) then
-		data(Index)
-		Res = ssma_TR()
-	
-
-	return tonumber(Settings.Horizontal_line), Res
-	end
-	
-end
-
-function data(Index)
-	
-	for i = 1, P*2 do
-
-		x = Index - 1 - (P * 2) + i
-			
-			TR[i] = math.max( math.max (H(x) - L(x), H(x) - L(x - 1)), math.abs(L(x)-C(x -1)) ) 
-			
-			if H(x) - H(x -1) > L(x -1) - L(x) then
-				DMplus[i] = math.max(H(x) - H(x -1), 0) 
-			else
-				DMplus[i] = 0	
-			end
-
-			if L(x-1) - L(x) > H(x) - H(x -1) then
-				DMminus[i] = math.max(L(x-1) - L(x), 0) 
-			else
-				DMminus[i] = 0	
-			end
-	end
-end
-
-
-function ssma_TR()
-	P = Settings.Period
-	local sum1 = 0
-	local sum2 = 0
-
-	for i=1, P*2 do
-		if i <= P / 2 then
-			sum1 = sum1 + TR[i]
-			sum1 = sum1 / (P/2)
-
-		else
-
-			sum2 = sum1
-			sum2 = ( (sum2*(P-1) + TR[i]) / P   
-	
-	end
-return sum2
-end
-
-function SMA_array(...)
-	local sum = 0
-	local arg = {...}
-	arg.n = select('#', ...)
-	for i=1, arg.n /2 do
-		sum = sum + arg[i]
-	end
-	sma = sum /(arg.n/2)
-	return sma
-
-end
-
-function SSMA_aray(...)
-
-	local arg = {...}
-	arg.n = select('#', ...)
-	
-	P = Settings.Period
-	local sum1 = 0
-	local sum2 = {}
+			for i = 1, P*2 do
 		
-	for i=1, arg.n do
-		if i <= P / 2 then
-			sum1 = sum1 + arg[i]
-			sum1 = sum1 / (arg.n/2)
-
-		else
-
-			sum2[1] = sum1
-			sum2[i - P] = ( (sum2[i-P-1]*(P-1) + arg[i]) / P   )
-		end
-
+				x = Index - 1 - (P * 2) + i
+					
+					TR[i] = math.max( math.max (H(x) - L(x), H(x) - L(x - 1)), math.abs(L(x)-C(x -1)) ) 
+					
+					if H(x) - H(x -1) > L(x -1) - L(x) then
+						DMplus[i] = math.max(H(x) - H(x -1), 0) 
+					else
+						DMplus[i] = 0	
+					end
 		
-	end
+					if L(x-1) - L(x) > H(x) - H(x -1) then
+						DMminus[i] = math.max(L(x-1) - L(x), 0) 
+					else
+						DMminus[i] = 0	
+					end
+				
+				if i > P then
 
-	return sum2[arg.n]
-end
+					DIplus = ssma(DMplus) / ssma(TR) * 100
+					DIminus = ssma(DMminus) / ssma(TR) * 100
+					DXi[i-P] = math.abs(DIplus - DIminus) / (DIplus + DIminus) * 100
+					
+				end
+			
+			end
+			
+			DIplus = ssma(DMplus) / ssma(TR) * 100
+			DIminus = ssma(DMminus) / ssma(TR) * 100
+			ADX = sma(DXi)
+			
+		
+			return tonumber(Settings.Horizontal_line), ADX, DIplus, DIminus
+        end
+        
+    end
+    
+    
+    
+    
+    function ssma(tab)
+        P = Settings.Period
+        local sum1 = 0
+        local sum2 = 0
+        local j = 0
+        
+    
+        for i,v in ipairs(tab) do
 
-function sma(fCandle, lCandle)
+            if i <= P then
+                sum1 = sum1 + v
+             else
+                if j==0 then
+                    sum2 = sum1 / P
+                    j=1
+                end
 
-	for i=fCandle , lCandle do
-	sum = sum + C(i)
-	end
-	sma = sum / (lCandle - fCandle)
+                sum2 =  (sum2*(P-1) + v) / P
+            end  
+        
+        end
+        return sum2
+    end
 
-	return sma
-
-end
-
-function ssma(Index)
-	sum1 = sma(Index - (Settings.Period * 2), Index - Settings.Period)
-	p = Settings.Period
-	sum2[1] = sum1
-
-	for i=2, P do
-		sum2[i] = ( sum2[i-1]*(P-1) + C(Index - (P-i)) ) / P
-	end
-
-	return sum2[P]
-
-end
+    function sma(tab)
+        local sum = 0
+        for i,v in ipairs(tab) do
+            sum = sum + v
+        end
+		result = sum/#tab
+		return result
+    end
+    
+    
